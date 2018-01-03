10e3462910ad07b5e8d7ba3191a87eb0728d7eff
garuscene <aganaluizgabriel08+github1@gmail.com>
2018-01-03 22:58:12 +0800

    Listed all songs

--- game.js
+++ game.js
@@ -1,4 +1,65 @@
+var songs = [
+//Self-Titled
+"Implicit Demand for Proof",
+"Fall Away",
+"The Pantaloon",
+"Addict With A Pen",
+"Friend, Please",
+"March To The Sea",
+"Johnny Boy",
+"Oh Ms Believer",
+"Air Catcher",
+"Trapdoor",
+"A Car, A Torch, A Death",
+"Taxi Cab",
+"Before You Start Your Day",
+"Isle Of Flightless Birds",
+//Regional at Best
+"Guns For Hands (RAB)",
+"Holding On To You (RAB)",
+"Ode To Sleep (RAB)",
+"Slowtown",
+"Car Radio (RAB)",
+"Forest",
+"Glowing Eyes",
+"Kitchen Sink",
+"Anathema",
+"Lovely (RAB)",
+"Ruby",
+"Trees (RAB)",
+"Be Concerned",
+"Clear",
+//Vessel
+"Ode To Sleep (V)",
+"Holding On To You (V)",
+"Migraine",
+"House of Gold (V)",
+"Car Radio (V)",
+"Semi-Automatic",
+"Screen",
+"The Run And Go",
+"Fake You Out",
+"Guns For Hands (V)",
+"Trees (V)",
+"Truce",
+//Blurryface
+"Heavydirtysoul",
+"Stressed Out",
+"Ride",
+"Fairly Local",
+"Tear In My Heart",
+"Lane Boy",
+"The Judge",
+"Doubt",
+"Polarize",
+"We Don't Believe What's On TV",
+"Message Man"
+"Hometown",
+"Not Today",
+"Goner"
+]
+
 function start() {
-	document.getElementById("removeOnStart").style.display = "none";
-	document.getElementById("showOnStart").style.display = "inline";
+	document.getElementById("start").style.display = "none";
+	document.getElementById("pick").style.display = "inline";
 }
